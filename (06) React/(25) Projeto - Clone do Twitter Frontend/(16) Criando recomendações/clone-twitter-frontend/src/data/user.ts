1ae94bc33a0c687690a2b720d050e563b418d359
Doug <kaido19@outlook.com>
2024-10-05 22:21:32 -0300

    Upload das aulas 15 à 20 do Modulo25 de React

---
+++ (06) React/(25) Projeto - Clone do Twitter Frontend/(16) Criando recomendações/clone-twitter-frontend/src/data/user.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+   slug: 'nick',
+   name: 'Nick Suzuki',
+   avatar: 'https://avatars.githubusercontent.com/u/108230681?v=4',
+   cover: 'https://media.licdn.com/dms/image/v2/D4D16AQGaQ9DDVtPzyw/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1709942479578?e=1733356800&v=beta&t=Fnn85UNqqRNWZWBu5Jdocvd7jsGvNXfFbek6J-LQH4U',
+   bio: 'Desenvolvedor Fullstack Javascript',
+   link: 'github.com/DouglasSuzukiDS',
+   postCount: 171
+}
---
+++ (06) React/(25) Projeto - Clone do Twitter Frontend/(20) Criando um tweet fake/clone-twitter-frontend/src/data/tweet.ts
@@ -0,0 +1,15 @@
+import { Tweet } from "@/types/tweet";
+import { user } from "./user";
+
+export const tweet: Tweet = {
+   id: 123,
+   user: user,
+   body: 'Banner',
+   image: 'https://media.licdn.com/dms/image/v2/D4D16AQGaQ9DDVtPzyw/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1709942479578?e=1733356800&v=beta&t=Fnn85UNqqRNWZWBu5Jdocvd7jsGvNXfFbek6J-LQH4U',
+   likeCount: 523,
+   commentsCount: 61,
+   retweetsCount: 0,
+   liked: true,
+   retweeted: false,
+   dataPost: new Date(2024, 8, 1, 10, 0, 0)
+} 

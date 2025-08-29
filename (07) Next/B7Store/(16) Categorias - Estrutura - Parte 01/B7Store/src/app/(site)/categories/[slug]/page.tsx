d90c329c992a9f927f7ccf77ea654c252188c3ca
Doug <kaido19@outlook.com>
2025-08-28 23:18:00 -0300

    Upload das aulas 16 à 24 do Frontend do Projeto B7Store

---
+++ (07) Next/B7Store/(16) Categorias - Estrutura - Parte 01/B7Store/src/app/(site)/categories/[slug]/page.tsx
@@ -0,0 +1,14 @@
+type Props = {
+   params: Promise<{ slug: string }>
+   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+export default async function Page({ params, searchParams }: Props) {
+   const { slug } = await params
+   const filters = await searchParams
+
+   return (
+      <div className="">
+         Slug: {slug}
+      </div>
+   )
+}

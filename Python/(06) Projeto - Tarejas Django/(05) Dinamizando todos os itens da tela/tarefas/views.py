ab573ce9ab6f3c79d09e134f630b04b437d9f77f
Doug <kaido19@outlook.com>
2026-01-31 23:25:08 -0300

    Uploas das aulas 02 à 06 do Modulo06 da Trilha de Python

---
+++ Python/(06) Projeto - Tarejas Django/(05) Dinamizando todos os itens da tela/tarefas/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Task
+
+def home (request):
+   tasks = Task.objects.all()
+   total_tasks = len(tasks)
+   completed_tasks = 0
+   remaining_tasks = 0
+   pct = 0
+
+   return render(
+      request, 
+      'tarefas/home.html',
+      {
+         'tasks': tasks,
+         'total': total_tasks,
+         'completed': completed_tasks,
+         'remaining': remaining_tasks,
+         'pct': pct
+      })
---
+++ Python/(06) Projeto - Tarejas Django/(06) Adicionando nova tarefa/tarefas/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def home (request):
+   tasks = Task.objects.all()
+   total_tasks = len(tasks)
+   completed_tasks = 0
+   remaining_tasks = 0
+   pct = 0
+
+   return render(
+      request, 
+      'tarefas/home.html',
+      {
+         'tasks': tasks,
+         'total': total_tasks,
+         'completed': completed_tasks,
+         'remaining': remaining_tasks,
+         'pct': pct
+      })
+
+def add(request):
+   form = TaskForm(request.POST)
+
+   if form.is_valid():
+      form.save()
+
+   return redirect('home')
